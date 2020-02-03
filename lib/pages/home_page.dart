11e994160de92cbada15799a04c8a0fad0b55ab5
unknown <987202055@qq.com>
2020-02-03 13:54:03 +0800

    实现顶部渐变效果

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+const APPBAR_SCROLL_OFFSET = 100;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,13 +8,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    "https://www.itying.com/images/flutter/1.png",
+    "https://www.itying.com/images/flutter/2.png",
+    "https://www.itying.com/images/flutter/3.png",
+  ];
+
+  double appBaralpha = 0;
+
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if(alpha<0){
+      alpha = 0;
+    }else if(alpha>1){
+      alpha = 1;
+    }
+    setState(() {
+      appBaralpha = alpha;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("首页"),
-      )
-    );
+        body: Stack(
+      children: <Widget>[
+        MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              // ignore: missing_return
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text("哈哈"),
+                    ),
+                  )
+                ],
+              ),
+            )),
+        Opacity(
+          opacity: appBaralpha,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text("首页"),
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
